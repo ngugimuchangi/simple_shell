8d3509722581562b6bcff68644f91e9095181d10
ngugimuchangi <tech.curious14@gmail.com>
2022-06-10 00:03:42 +0000

    Initial commit

---
+++ main.h
@@ -0,0 +1,47 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define PATH "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+#define EXEC 0
+#define SEQ 1
+#define AND 2
+#define OR 3
+
+extern char **environ;
+
+/**
+ * struct list - structure of commands and arguments
+ * @cmd: command to execute
+ * @arg: arguments for the commands
+ * @next: pointer to the next list
+ *
+ * Description: command linked list node structure
+ */
+typedef struct list
+{
+	char *cmd;
+	char **arg;
+	list *next;
+} cmd_list;
+
+void prompt(void);
+unsigned int logic_tree(char **s);
+int parse_path(char *s1, char **s2);
+ssize_t _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+char *_strtok(char *str, const char *delim);
+void _strcat(char *s1, char *s2, char *des);
+ssize_t _getline(char **lineptr, size_t *n);
+void *_memset(void *s, int c, size_t n);
+void cd(char *s);
+void _fork(void);
+void err(char *s);
+
+#endif
---
+++ shell.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main - shell entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+}
