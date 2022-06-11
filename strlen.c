07334df3398b3adfd72bbb4ca4a7fa608181b203
Sgechuki <sgechuki@outlook.com>
2022-06-11 02:25:44 -0700

    Find length of string

---
+++ strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen -Calculate length of string
+ * @s: pointer to string
+ * Return: length of string
+ */
+ssize_t _strlen(const char *s)
+{
+	ssize_t i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
