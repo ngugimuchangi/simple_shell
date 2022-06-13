ce9d04a13d7d5432922848a75ca6e4ef9942c527
ngugimuchangi <tech.curious14@gmail.com>
2022-06-13 18:17:06 +0000

    Minor edit

--- isatty.c
+++ isatty.c
@@ -3,16 +3,13 @@
 /**
  * _isatty - tells you if the standard input are associated with terminal
  * @mode: pointer
- * Return:
+ *
+ * Return: nothing
  */
 void _isatty(int *mode)
 {
 	if (!isatty(0))
-	{
 		*mode = 0;
-	}
 	else
-	{
 		write(STDOUT_FILENO, "\033[0;32msimple_shell:$ \033[0;37m", 30);
-	}
 }
